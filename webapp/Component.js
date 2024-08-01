60d4bde5002c28caf8ab716f00ec3ef1700710ea
Pudota Mary Harika <pudota.maryharika@ust.com>
2024-08-01 19:17:16 +0000

    first changes

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("trav.ust.hp.travelproc.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
